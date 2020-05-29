6f503ca1e990b9b7dc20af1e0b3fec877a896ae6
Prateek Rawat <prateekrawat33@gmail.com>
2020-05-29 22:43:13 +0530

    Updates

---
+++ precache-manifest.a1430594faf5cf3756648dc9ab4295a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8652718fa33aa59da3c8076389c6b0e",
+    "url": "/Restaurant-management-using-react-redux/index.html"
+  },
+  {
+    "revision": "21f3d3d42fbfbe1c8c27",
+    "url": "/Restaurant-management-using-react-redux/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "21f3d3d42fbfbe1c8c27",
+    "url": "/Restaurant-management-using-react-redux/static/js/2.8d7b2132.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Restaurant-management-using-react-redux/static/js/2.8d7b2132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e132d2fafc433c29e8ba",
+    "url": "/Restaurant-management-using-react-redux/static/js/main.b1c1eb8a.chunk.js"
+  },
+  {
+    "revision": "09a28761bf3abc9eed28",
+    "url": "/Restaurant-management-using-react-redux/static/js/runtime-main.8ebf8409.js"
+  }
+]);
